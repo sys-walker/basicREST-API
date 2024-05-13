ee4d5b1aedbceb32fd8e486e1e00687cd8db6514
USE-IT SL <parollon@useit.es>
2024-05-13 10:59:24 +0200

    removed phpmyadmin files and replaced to docker volume

--- web/views/parts/sidenav.php
+++ web/views/parts/sidenav.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
   <a class="active" href="/">Home</a>
   <a href="/use">Api instructions</a>
-  <a href="/phpMyAdmin">phpMyAdmin</a>
+  <a href="http://localhost:8081">phpMyAdmin</a>
   <a href="/info">PHP info</a>
   <a href="#about">About</a>
 </div>
